8ddeac10236506ad16b20948466fcdd6277abfd7
Julio Trecenti <julio.trecenti@gmail.com>
2016-07-10 23:05:35 -0500

    atualizacao na api

--- R/cnc.R
+++ R/cnc.R
@@ -1,6 +1,6 @@
 #' @import dplyr
 #' @export
-parse_cnc_pag <- function(arq) {
+parse_pag <- function(arq) {
   arq %>%
     xml2::read_html() %>%
     rvest::html_nodes('table') %>%
@@ -26,68 +26,12 @@ parse_cnc_pag <- function(arq) {
     }
 }
 
-cnc_pag <- function(p, path, parse = TRUE) {
-  x <- 'http://www.cnj.jus.br/improbidade_adm/consultar_requerido.php?validar=form&rs=pesquisarRequeridoGetTabela&rst=&rsrnd=0&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=I&rsargs[]=0&rsargs[]=POSICAO_INICIAL_PAGINACAO_PHP%s&rsargs[]=QUANTIDADE_REGISTROS_PAGINACAO15'
-  u <- sprintf(x, as.character((p - 1) * 15))
-  arq <- sprintf('%s/pag_%05d.html', path, p)
-  if (!file.exists(arq)) {
-    r <- httr::GET(u, httr::write_disk(arq, overwrite = TRUE))
-    if (parse) {
-      r %>% httr::content('text') %>% parse_cnc_pag()
-    } else {
-      r
-    }
-  } else {
-    dplyr::data_frame(result = 'arquivo existe')
-  }
-}
-
-cnc_pags <- function(path = 'data-raw/pags', parse = TRUE) {
-  dir.create(path, showWarnings = FALSE, recursive = TRUE)
-  dplyr::data_frame(pag = 1:2399) %>%
-    dplyr::group_by(pag) %>%
-    dplyr::do(cnc_pag(.$pag, path)) %>%
-    dplyr::ungroup()
-}
-
-cnc_condenacao <- function(dados, path = 'data-raw/pessoas') {
-  dir.create(path, showWarnings = FALSE, recursive = TRUE)
-  dados %>%
-    dplyr::filter(key == 'nm_pessoa') %>%
-    dplyr::distinct(link) %>%
-    dplyr::mutate(num_link = gsub('[^0-9]', '', link)) %>%
-    dplyr::group_by(link, num_link) %>%
-    dplyr::do({
-      l <- paste0('http://www.cnj.jus.br/improbidade_adm/', .$link)
-      a <- sprintf('%s/%s.html', path, .$num_link)
-      if (!file.exists(a)) {
-        # httr::GET(l, httr::write_disk(a, overwrite = TRUE))
-        GET_pjs(l, a)
-        dplyr::data_frame(result = 'OK')
-      } else {
-        dplyr::data_frame(result = 'ja existe')
-      }
-    }) %>%
-    dplyr::ungroup()
-}
-
-GET_pjs <- function(url, arq = 'arq.html') {
-  tmp <- tempfile('scrape', fileext = '.js')
-  writeLines(sprintf("var page = require('webpage').create();
-    page.open('%s', function () {
-        console.log(page.content); //page source
-        phantom.exit();
-    });", url), con = tmp)
-  system(sprintf("phantomjs %s > %s", tmp, arq))
-  file.remove(tmp)
-}
-
 #' @import xml2
 #' @import rvest
 #' @import stringr
 #' @import dplyr
 #' @export
-parse_condenacao_um <- function(arq) {
+parse_pessoa_um <- function(arq) {
   # data de cadastro
   h <- read_html(arq)
   dt_cad <- h %>%
@@ -166,7 +110,113 @@ parse_condenacao_um <- function(arq) {
     bind_rows(assunto)
 
   bind_rows(tab_processo, tab_pessoa, tab_condenacao)
+}
 
+parse_processo_um <- function(arq) {
+  h <- read_html(arq)
+  tab_processo <- h %>%
+    html_nodes('#hierarquia > div') %>%
+    html_text() %>%
+    str_replace_all("[[:space:]]+", " ") %>%
+    str_trim() %>%
+    str_split_fixed(':', 2) %>%
+    data.frame(stringsAsFactors = FALSE) %>%
+    tbl_df() %>%
+    setNames(c('key', 'value')) %>%
+    mutate_all(funs(str_trim(.)))
+
+  tab_condenacao <- h %>%
+    html_node(xpath = '//table[@width="700px" and @align="center"]') %>%
+    html_table(fill = TRUE) %>%
+    filter(str_detect(X1, 'Num. do|Data da')) %>%
+    select(key = X1, value = X2) %>%
+    mutate(key = gsub(':', '', key))
+  bind_rows(tab_processo, tab_condenacao)
+}
+
+#' @import stringr
+#' @export
+parse_infos_pessoa_um <- function(arq) {
+  h <- read_html(arq)
+  s <- h %>%
+    html_text() %>%
+    str_match('res = \'(.*?)\';') %>%
+    as.data.frame(stringsAsFactors = FALSE) %>%
+    with(V2) %>%
+    str_split(',') %>%
+    unlist()
+  dplyr::data_frame(
+    id = s[1],
+    tipo_pessoa = s[2],
+    nm_pessoa = s[3],
+    sexo = s[4],
+    publico = s[5],
+    esfera = s[6],
+    orgao = s[7],
+    cargo = s[8],
+    uf = s[9],
+    cod = s[12]
+  ) %>%
+    mutate(uf = ifelse(publico == 'N', '', uf),
+           sexo = ifelse(tipo_pessoa == 'J', '', tipo_pessoa))
+}
+
+# -----------------------------------------------------------------------------
+
+GET_pjs <- function(url, arq = 'arq.html') {
+  tmp <- tempfile('scrape', fileext = '.js')
+  writeLines(sprintf("var page = require('webpage').create();
+    page.open('%s', function () {
+        console.log(page.content); //page source
+        phantom.exit();
+    });", url), con = tmp)
+  system(sprintf("phantomjs %s > %s", tmp, arq))
+  file.remove(tmp)
+}
+
+cnc_pag <- function(p, path, parse = TRUE) {
+  x <- 'http://www.cnj.jus.br/improbidade_adm/consultar_requerido.php?validar=form&rs=pesquisarRequeridoGetTabela&rst=&rsrnd=0&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=I&rsargs[]=0&rsargs[]=POSICAO_INICIAL_PAGINACAO_PHP%s&rsargs[]=QUANTIDADE_REGISTROS_PAGINACAO15'
+  u <- sprintf(x, as.character((p - 1) * 15))
+  arq <- sprintf('%s/pag_%05d.html', path, p)
+  if (!file.exists(arq)) {
+    r <- httr::GET(u, httr::write_disk(arq, overwrite = TRUE))
+    if (parse) {
+      r %>% httr::content('text') %>% parse_cnc_pag()
+    } else {
+      r
+    }
+  } else {
+    dplyr::data_frame(result = 'arquivo existe')
+  }
+}
+
+cnc_pags <- function(path = 'data-raw/pags', parse = TRUE) {
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  dplyr::data_frame(pag = 1:2399) %>%
+    dplyr::group_by(pag) %>%
+    dplyr::do(cnc_pag(.$pag, path)) %>%
+    dplyr::ungroup()
+}
+
+cnc_condenacao <- function(dados, path = 'data-raw/pessoas') {
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  dados %>%
+    dplyr::filter(key == 'nm_pessoa') %>%
+    dplyr::distinct(link) %>%
+    dplyr::mutate(num_link = gsub('[^0-9]', '', link)) %>%
+    dplyr::group_by(link, num_link) %>%
+    dplyr::do({
+      l <- paste0('http://www.cnj.jus.br/improbidade_adm/', .$link)
+      a <- sprintf('%s/%s.html', path, .$num_link)
+      if (!file.exists(a)) {
+        # httr::GET(l, httr::write_disk(a, overwrite = TRUE))
+        GET_pjs(l, a)
+        dplyr::data_frame(result = 'OK')
+      } else {
+        dplyr::data_frame(result = 'ja existe')
+      }
+    }) %>%
+    dplyr::ungroup()
 }
 
 cnc_processo <- function(dados, path = 'data-raw/processos') {
@@ -190,7 +240,22 @@ cnc_processo <- function(dados, path = 'data-raw/processos') {
     dplyr::ungroup()
 }
 
-parse_processo <- function() {
-
+cnc_pessoas_infos <- function(id_pessoa, path = 'data-raw/pessoas_infos') {
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  l <- 'http://www.cnj.jus.br/improbidade_adm/visualizar_condenacao.php?seq_condenacao=1&rs=getDadosParte&rst=&rsrnd=0&rsargs[]=%s'
+  l <- sprintf(l, id_pessoa)
+  dplyr::data_frame(id = id_pessoa, link = l) %>%
+    dplyr::group_by(id, link) %>%
+    dplyr::do({
+      a <- sprintf('%s/%s.html', path, .$id)
+      if (!file.exists(a)) {
+        httr::GET('http://www.cnj.jus.br/improbidade_adm/visualizar_condenacao.php?seq_condenacao=1')
+        httr::GET(.$link, httr::write_disk(a, overwrite = TRUE))
+        dplyr::data_frame(result = 'OK')
+      } else {
+        dplyr::data_frame(result = 'ja existe')
+      }
+    }) %>%
+    dplyr::ungroup()
 }
 
